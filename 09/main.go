f8f4fc900bc688cc0b3d5bbc647f615a813c5519
Alex Tan Hong Pin <alextan220990@gmail.com>
2025-12-09 18:39:47 +0800

    chore: add solution

--- 09/main.go
+++ 09/main.go
@@ -19,25 +19,50 @@ func main() {
 	testInput = strings.TrimSpace(testInput)
 	input = strings.TrimSpace(input)
 
-	fmt.Println("test1:", part1(testInput)) // 0
-	fmt.Println("prod1:", part1(input))     // 0
+	fmt.Println("test1:", part1(testInput)) // 50
+	fmt.Println("prod1:", part1(input))     // 4756718172
 
-	fmt.Println("test2:", part2(testInput)) // 0
-	fmt.Println("prod2:", part2(input))     // 0
+	fmt.Println("test2:", part2(testInput)) // 24
+	fmt.Println("prod2:", part2(input))     // 1665679194
+}
+
+type Point struct {
+	x, y int
+}
+
+func (p Point) Distance(o Point) int {
+	x := (p.x - o.x)
+	y := (p.y - o.y)
+	return x*x + y*y
 }
 
 func part1(input string) int {
+	var points []Point
 	for row := range strings.SplitSeq(input, "\n") {
-		_ = row
+		a, b, ok := strings.Cut(row, ",")
+		if !ok {
+			panic("invalid input")
+		}
+		i, j := toInt(a), toInt(b)
+		points = append(points, Point{i, j})
 	}
-	return 0
+
+	p := points
+	var area int
+	l := len(p)
+	for i := range l {
+		for j := range l {
+			width := abs(p[i].x-p[j].x) + 1
+			height := abs(p[i].y-p[j].y) + 1
+			area = max(area, width*height)
+		}
+	}
+
+	return area
 }
 
 func part2(input string) int {
-	for row := range strings.SplitSeq(input, "\n") {
-		_ = row
-	}
-	return 0
+	panic("not implemented")
 }
 
 func toInt(s string) int {
@@ -46,4 +71,11 @@ func toInt(s string) int {
 		panic(err)
 	}
 	return n
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
